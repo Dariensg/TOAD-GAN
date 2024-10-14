872c807ac09240caacad2eae77719af57a3d5bb9
Darien Gillespie <dsgillespie02@yahoo.com>
2024-10-13 19:49:42 -0500

    Shape prints for debugging

--- models/generator.py
+++ models/generator.py
@@ -34,6 +34,8 @@ class Level_GeneratorConcatSkip2CleanAdd(nn.Module):
         x = self.tail(x)
 
         x_shape = x.shape
+        print("x shape:", x.shape)
+        print("x flatten shape:", x.flatten().shape)
         x = self.fc(x.flatten())
         x = x.reshape(x_shape)
 
