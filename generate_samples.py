Fix scaling and newlines

generate_samples.py
@@ -410,7 +410,7 @@ if __name__ == '__main__':
 
             # Downsample "other level"
             real_fakes_down = special_mario_downsampling(1, [[opt.scales[-1], opt.scales[-1]]],
-                                                         real_fakes, opt_fakes.token_list, opt.repr_type, opt.use_hierarchy)
+                                                         real_fakes, opt_fakes.token_list, opt.repr_type, opt.block2repr, opt.use_hierarchy)
 
             run_dir = "/home/awiszus/Project/TOAD-GAN/wandb/"
             if seed_level == 0:  # Only done for mario levels 1 to 3 so far
@@ -439,7 +439,7 @@ if __name__ == '__main__':
 
         else:
             # Get input shape for in_s
-            real_down = downsample(1, [[opt.scale_v, opt.scale_h]], real, opt.token_list)
+            real_down = downsample(1, [[opt.scale_v, opt.scale_h]], real, opt.token_list, opt.repr_type, opt.block2repr, opt.use_hierarchy)
             real_down = real_down[0]
             in_s = torch.zeros_like(real_down, device=opt.device)
             prefix = "arbitrary"

main.py
@@ -96,7 +96,7 @@ def main():
     logger.info("Generating arbitrary sized random samples...")
     scale_v = 0.8  # Arbitrarily chosen scales
     scale_h = 0.4
-    real_down = downsample(1, [[scale_v, scale_h]], generator_real, opt.token_list, opt.repr_type, opt.use_hierarchy)
+    real_down = downsample(1, [[scale_v, scale_h]], generator_real, opt.token_list, opt.repr_type, opt.block2repr, opt.use_hierarchy)
     real_down = real_down[0]
     # necessary for correct input shape
     in_s = torch.zeros(real_down.shape, device=opt.device)

mario/level_utils.py
@@ -70,9 +70,9 @@ def ascii_to_block2vec_level(level, dim, repr):
 
     return oh_level
 
-def encoded_to_ascii_level(level, tokens, repr, repr_type):
+def encoded_to_ascii_level(level, tokens, block2repr, repr_type):
     if repr_type == 'block2vec':
-        return block2vec_to_ascii_level(level, repr)
+        return block2vec_to_ascii_level(level, block2repr)
     else:
         return one_hot_to_ascii_level(level, tokens)
 
@@ -89,9 +89,9 @@ def one_hot_to_ascii_level(level, tokens):
         ascii_level.append(line)
     return ascii_level
 
-def block2vec_to_ascii_level(level, repr):
+def block2vec_to_ascii_level(level, block2repr):
     # Convert block2vec level into 2D array of block indices
-    block_embs = torch.stack(list(repr.values()))
+    block_embs = torch.stack(list(block2repr.values()))
     world_vec = level.squeeze().permute(1,2,0).unsqueeze(3)
     blocks_vec = block_embs.permute(1,0)[None,None,...]
     dist = (world_vec - blocks_vec).pow(2).sum(dim=-2)
@@ -101,8 +101,8 @@ def block2vec_to_ascii_level(level, repr):
     for i in range(labels.shape[0]):
         line = ""
         for j in range(labels.shape[1]):
-            line += list(repr.keys())[labels[i, j]]
-        if i < level.shape[0] - 1:
+            line += list(block2repr.keys())[labels[i, j]]
+        if i < labels.shape[0] - 1:
             line += "\n"
         ascii_level.append(line)
 

mario/special_mario_downsampling.py
@@ -3,10 +3,10 @@ from torch.nn.functional import interpolate
 from torch.nn import Softmax
 
 from .tokens import TOKEN_DOWNSAMPLING_HIERARCHY as HIERARCHY
-from mario.level_utils import encoded_to_ascii_level
+from mario.level_utils import encoded_to_ascii_level, block2vec_to_ascii_level, ascii_to_one_hot_level, ascii_to_block2vec_level, one_hot_to_ascii_level
 
 
-def special_mario_downsampling(num_scales, scales, image, token_list, repr_type, use_hierarchy=False):
+def special_mario_downsampling(num_scales, scales, image, token_list, repr_type, block2repr, use_hierarchy=False):
     """
     Special Downsampling Method designed for Super Mario Bros. Token based levels.
 
@@ -16,19 +16,20 @@ def special_mario_downsampling(num_scales, scales, image, token_list, repr_type,
     token_list : list of tokens appearing in the image in order of channels from image.
     """
 
+    dim = len(token_list)
+    if repr_type == 'block2vec':
+        dim = image.shape[1]
+        image = ascii_to_one_hot_level(block2vec_to_ascii_level(image, block2repr), token_list)
+        image = image.unsqueeze(0)
+
     scaled_list = []
     for sc in range(num_scales):
         scale_v = scales[sc][0]
         scale_h = scales[sc][1]
 
-        if repr_type == 'block2vec':
-            # Initial downscaling of block2vec level tensor
-            bil_scaled = interpolate(image, (int(image.shape[-2] * scale_v), int(image.shape[-1] * scale_h)), 
-                                     mode='bilinear', align_corners=False)
-        else:
-            # Initial downscaling of one-hot level tensor is normal bilinear scaling
-            bil_scaled = interpolate(image, (int(image.shape[-2] * scale_v), int(image.shape[-1] * scale_h)),
-                                    mode='bilinear', align_corners=False)
+        # Initial downscaling of one-hot level tensor is normal bilinear scaling
+        bil_scaled = interpolate(image, (int(image.shape[-2] * scale_v), int(image.shape[-1] * scale_h)),
+                                mode='bilinear', align_corners=False)
 
         # Init output level
         img_scaled = torch.zeros_like(bil_scaled)
@@ -44,19 +45,20 @@ def special_mario_downsampling(num_scales, scales, image, token_list, repr_type,
                             if token in curr_tokens:
                                 curr_h = h
 
-                if repr_type == 'block2vec':
-                    img_scaled = bil_scaled
-                else:
-                    for t in range(bil_scaled.shape[-3]):
-                        if use_hierarchy and not (token_list[t] in HIERARCHY[curr_h].keys()):
-                            # if this token is not on the correct hierarchy group, set to 0
-                            img_scaled[:, t, x, y] = 0
-                        else:
-                            # if it is, keep original value
-                            img_scaled[:, t, x, y] = bil_scaled[:, t, x, y]
+                for t in range(bil_scaled.shape[-3]):
+                    if use_hierarchy and not (token_list[t] in HIERARCHY[curr_h].keys()):
+                        # if this token is not on the correct hierarchy group, set to 0
+                        img_scaled[:, t, x, y] = 0
+                    else:
+                        # if it is, keep original value
+                        img_scaled[:, t, x, y] = bil_scaled[:, t, x, y]
 
-                    # Adjust level to look more like the generator output through a Softmax function.
-                    img_scaled[:, :, x, y] = Softmax(dim=1)(30*img_scaled[:, :, x, y])
+                # Adjust level to look more like the generator output through a Softmax function.
+                img_scaled[:, :, x, y] = Softmax(dim=1)(30*img_scaled[:, :, x, y])
+
+        if repr_type == 'block2vec':
+            img_scaled = ascii_to_block2vec_level(one_hot_to_ascii_level(img_scaled, token_list), dim, block2repr)
+            img_scaled = img_scaled.unsqueeze(0)
 
         scaled_list.append(img_scaled)
 